6868f9b3a760e36bde92e8130fc6ccbeefb0024e
MURA199918 <muralisaranr18@gmail.com>
2020-12-14 10:53:05 +0530

    [Murali]Added test case for given distance and time should return fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,7 +1,10 @@
 public class InvoiceGenerator {
 
+    private static final double MIN_COST_PER_KM = 10.0;
+    private static final int MIN_COST_PER_TIME = 1;
+
     public double calculateFare(double distance, int time) {
-        double cost = (distance*10) + (time*1);
+        double cost = (distance*MIN_COST_PER_KM) + (time*MIN_COST_PER_TIME);
         return cost;
     }
 }
